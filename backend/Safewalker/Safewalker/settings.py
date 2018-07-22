edeebf74a577324ac048687c4aae28798d2d363c
kismet-cruz <yipinglu@umail.ucsb.edu>
2018-07-22 11:58:59 +0800

    setting:csrf

--- backend/Safewalker/Safewalker/settings.py
+++ backend/Safewalker/Safewalker/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework.authtoken',
 ]
 
@@ -53,7 +54,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    'ParentsChild.csrf.DisableCsrfCheck',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    # 'ParentsChild.csrf.DisableCsrfCheck',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +134,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CORS_ORIGIN_ALLOW_ALL=True
